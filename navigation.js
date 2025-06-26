66a71f902bd287cb13ce23193eddb59cfaf954e5
SMANSA <taekwondo.smansa.kota.sorong@gmail.com>
2025-06-26 17:06:27 +0900

    Update navigation.js

--- navigation.js
+++ navigation.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('kte1-btn').addEventListener('click', function() {
-        window.location.href = 'pesan_dari_herman_ke_pak_budi.htm';
+        window.location.href = 'pesan_dari_herman_ke_pak_budi.html';
     });
     
     document.getElementById('kte2-btn').addEventListener('click', function() {
